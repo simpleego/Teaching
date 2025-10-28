d3dcf6f41218b301a8d726e6f4fa43e1218612c0
Jong Cheon Park <simpleego@gmail.com>
2025-10-29 07:58:43 +0900

    Add files via upload

---
+++ React/영화예매/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import Reserve from './pages/Reserve';
+import NotFound from './pages/NotFound';
+import MovieDetail from './pages/MovieDetail'; // MovieDetail 컴포넌트 import
+
+import Signup from './pages/Signup'; // Signup 컴포넌트 import
+import Login from './pages/Login';   // Login 컴포넌트 import
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetail />} />
+          <Route path="reserve" element={<Reserve />} />
+          <Route path="signup" element={<Signup />} /> {/* 회원가입 경로 추가 */}
+          <Route path="login" element={<Login />} />   {/* 로그인 경로 추가 */}
+        </Route>
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ React/영화예매/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>메인 페이지</h1>
+      <p>오신 것을 환영합니다! 상단 메뉴를 통해 이동하세요.</p>
+    </div>
+  );
+};
+
+export default Home;
