aff618a20f853f46b316bd55745117ebbda238de
Jong Cheon Park <simpleego@gmail.com>
2025-11-02 15:41:42 +0900

    Add files via upload

--- React/영화예매/Final_Source/src/components/MovieForm.js
+++ React/영화예매/Final_Source/src/components/MovieForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './MovieForm.css';
 
 const MovieForm = ({ onSubmit, initialData = {} }) => {
     const [movie, setMovie] = useState({
@@ -37,12 +38,12 @@ const MovieForm = ({ onSubmit, initialData = {} }) => {
 
     return (
         <form onSubmit={handleSubmit} className="movie-form">
-            <input name="title" value={movie.title} onChange={handleChange} placeholder="제목" required />
-            <input name="genre" value={movie.genre} onChange={handleChange} placeholder="장르" required />
-            <input name="director" value={movie.director} onChange={handleChange} placeholder="감독" required />
-            <input name="releaseDate" type="date" value={movie.releaseDate} onChange={handleChange} required />
-            <textarea name="description" value={movie.description} onChange={handleChange} placeholder="설명" required />
-            <input type="file" onChange={handleFileChange} />
+            <label>영화제목</label><input name="title" value={movie.title} onChange={handleChange} placeholder="제목" required />
+            <label>영화장르</label><input name="genre" value={movie.genre} onChange={handleChange} placeholder="장르" required />
+            <label>영화감독</label><input name="director" value={movie.director} onChange={handleChange} placeholder="감독" required />
+            <label>영화출시일</label><input name="releaseDate" type="date" value={movie.releaseDate} onChange={handleChange} required />
+            <label>영화이야기</label><textarea rows="5" cols="40" name="description" value={movie.description} onChange={handleChange} placeholder="설명" required />
+            <label>영화포스터</label><input type="file" onChange={handleFileChange} />
             {initialData.poster && !posterFile && <img src={initialData.poster} alt="Poster" width="100" />}
             <button type="submit">{initialData.id ? '수정하기' : '등록하기'}</button>
         </form>
